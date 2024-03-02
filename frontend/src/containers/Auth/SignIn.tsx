a4e912df4d070b551bafbcdbd523623c99855616
Akshay Racch <akshayracch@Akshays-MacBook-Pro-2.local>
2024-03-01 21:52:45 -0500

    LogIn Error Handling Implementation

--- frontend/src/containers/Auth/SignIn.tsx
+++ frontend/src/containers/Auth/SignIn.tsx
@@ -5,15 +5,68 @@ import { useAuth } from "../../services/auth";
 import styles from "./auth.module.css";
 
 const SignIn = () => {
+  const { authenticated, signIn } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { authenticated, signIn } = useAuth();
+  const [error,setError] = useState<Record<string, string>>({}); 
+  const [isValid, setValidation] = useState(false);
   const navigate = useNavigate();
+  
 
-  const handleSignIn = () => {
-    signIn(email, password);
+  const checkEmail = (email: string): boolean => {
+    // You can use a regex or any other validation method here
+    const regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
   };
 
+  const checkPwd = (password: string): boolean => {
+    const regex = /^(?=.*[A-Z]).{8,}$/;
+    return regex.test(password);
+  };
+
+  const checkValid = () => {
+    const newError: Record<string, string> = {};
+
+    if (!email.trim()) {
+      newError.email = "*Email is required*";
+    } else if (!checkEmail(email)) {
+      newError.email = "*Email address should contain '@'*";
+    }
+
+    if (!checkPwd(password)) {
+      newError.password = "*Password should have at least 8 characters and one upper case letter*";
+    }
+
+    setError(newError);
+    const isValid = Object.keys(newError).length === 0;
+    setValidation(isValid);
+    // return isValid;
+  };
+  const handleEmail = (email: string)=> {
+    setEmail(email);
+    checkValid();
+  }
+
+  const handlePassword = (password: string) => {
+    setPassword(password);
+    checkValid();
+  }
+
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    checkValid();
+
+    if(isValid){
+      try {
+        signIn(email, password);
+        setValidation(true);
+        setError({});
+
+      } catch (error:any) {
+        setError({ general: error.message});
+      }
+    }
+  };
   useEffect(() => {
     if (authenticated) {
       navigate("/dashboard", { replace: true });
@@ -30,21 +83,33 @@ const SignIn = () => {
       <div className={styles.authContainer}>
         <div className={styles.authCard}>
           <h1 className={styles.authTitle}>Welcome back</h1>
+
+          <form action ="" onSubmit = {handleSignIn}>
+
           <input
             type="email"
             className={styles.authInput}
             placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(event) => handleEmail(event.target.value)}
           />
+          {error && <p className={styles.authError}>{error.email}</p>}
+
           <input
             type="password"
             className={styles.authInput}
             placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(event) => handlePassword(event.target.value)}
           />
-          <button className={styles.authButton} onClick={handleSignIn}>
+          {error && <p className={styles.authError}>{error.password}</p>}
+
+          <button 
+            type="submit" 
+            className={`${styles.authButton} ${isValid ? styles.authButton : styles.disabledButton}`}
+            disabled={!isValid}
+            >
             Log in
           </button>
+          </form>
           <footer className={styles.authFooter}>
             Don't have an account?{" "}
             <Link to="/signup" className={styles.authToggle}>
