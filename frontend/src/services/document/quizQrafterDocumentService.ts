a961c54f9f56e7dc29f91bcd1d0a90ce29e86189
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-04-12 21:22:35 -0400

    [frontend] integrate document upload

--- frontend/src/services/document/quizQrafterDocumentService.ts
+++ frontend/src/services/document/quizQrafterDocumentService.ts
@@ -4,6 +4,12 @@ export class QuizQrafterDocumentService implements DocumentService {
   constructor(private readonly baseURL: URL) {}
 
   async upload(file: File): Promise<void> {
-    // TODO: implement this later
+    const data = new FormData();
+    data.append("file", file, file.name);
+    await fetch(new URL("/api/v1/document/upload", this.baseURL), {
+      method: "POST",
+      credentials: "include",
+      body: data,
+    });
   }
 }
