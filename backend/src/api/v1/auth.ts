9cc02b87f0fa3dbf8c5407da0ba34788f0e2f674
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-04-12 20:25:51 -0400

    add error-handling to routes
    handle errors in route endpoints to prevent server from crashing

--- backend/src/api/v1/auth.ts
+++ backend/src/api/v1/auth.ts
@@ -4,85 +4,105 @@ import prisma from "../../database";
 const router = Router();
 
 router.post("/signup", async (request, response) => {
-  const { email, password, fullname } = request.body;
+  try {
+    const { email, password, fullname } = request.body;
 
-  if (!email || !password || !fullname) {
-    return response.sendStatus(400 /* Bad Request */);
-  }
-
-  const user = await prisma.user.create({
-    data: {
-      email: email,
-      password: password,
-      fullname: fullname,
-    },
-  });
-
-  return response.status(200 /* OK */).json({
-    id: user.id,
-    email: user.email,
-    fullname: user.fullname,
-  });
-});
-
-router.post("/login", async (request, response) => {
-  const { email, password } = request.body;
-
-  if (!email || !password) {
-    return response.sendStatus(400 /* Bad Request */);
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { email: email },
-  });
-
-  if (user === null || user.password !== password) {
-    return response.sendStatus(404 /* Not Found */);
-  }
-
-  request.session.regenerate((error) => {
-    if (error) {
-      console.error("Error in [/login]:", error);
-      return response.sendStatus(500 /* Internal Server Error */);
+    if (!email || !password || !fullname) {
+      return response.sendStatus(400 /* Bad Request */);
     }
-    // @ts-ignore
-    request.session.userId = user.id; // set the ID to know which user is making a request next time
 
-    return response.status(200).json({
+    const user = await prisma.user.create({
+      data: {
+        email: email,
+        password: password,
+        fullname: fullname,
+      },
+    });
+
+    return response.status(200 /* OK */).json({
       id: user.id,
       email: user.email,
       fullname: user.fullname,
     });
-  });
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
+});
+
+router.post("/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+      return response.sendStatus(400 /* Bad Request */);
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (user === null || user.password !== password) {
+      return response.sendStatus(404 /* Not Found */);
+    }
+
+    request.session.regenerate((error) => {
+      if (error) {
+        console.error("Error in [/login]:", error);
+        return response.sendStatus(500 /* Internal Server Error */);
+      }
+      // @ts-ignore
+      request.session.userId = user.id; // set the ID to know which user is making a request next time
+
+      return response.status(200).json({
+        id: user.id,
+        email: user.email,
+        fullname: user.fullname,
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
 });
 
 router.post("/logout", (request, response) => {
-  request.session.destroy((error) => {
-    if (error) {
-      console.error("Error in [/logout]:", error);
-      return response.sendStatus(500);
-    }
+  try {
+    request.session.destroy((error) => {
+      if (error) {
+        console.error("Error in [/logout]:", error);
+        return response.sendStatus(500);
+      }
 
-    return response.sendStatus(200);
-  });
+      return response.sendStatus(200);
+    });
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
 });
 
 router.get("/info", restrict, async (req, res) => {
-  // @ts-ignore
-  const userId = req.session.userId;
+  try {
+    // @ts-ignore
+    const userId = req.session.userId;
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
 
-  if (user === null) return res.sendStatus(404 /* Not Found */);
+    if (user === null) return res.sendStatus(404 /* Not Found */);
 
-  return res.status(200).json({
-    email: user.email,
-    fullname: user.fullname,
-  });
+    return res.status(200).json({
+      email: user.email,
+      fullname: user.fullname,
+    });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 export function restrict(
--- backend/src/api/v1/quiz.ts
+++ backend/src/api/v1/quiz.ts
@@ -15,32 +15,33 @@ For formatting response, set Accept request header to
 - (Markdown) Accept: text/markdown
 */
 router.post("/new", restrict, async (req, res) => {
-  // @ts-ignore
-  const userId = req.session.userId;
-  const user = await prisma.user.findUnique({ where: { id: userId } });
-  if (user === null) {
-    // user does not exist
-    console.error(`User-${userId} does not exist`);
-    return res.sendStatus(401 /* Unauthorized */);
-  }
+  try {
+    // @ts-ignore
+    const userId = req.session.userId;
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (user === null) {
+      // user does not exist
+      console.error(`User-${userId} does not exist`);
+      return res.sendStatus(401 /* Unauthorized */);
+    }
 
-  const filename = req.body.filename;
+    const filename = req.body.filename;
 
-  // Upload raw document and transcripted file to bucket
-  const storage = new Storage({ keyFile: GCLOUD_KEY_FILEPATH! });
-  const bucket = storage.bucket("quizqrafter-documents");
-  const transcriptedFile = bucket.file(`u${userId}-${filename}.transcript`);
-  const dbDoc = await prisma.document.findFirst({
-    where: {
-      transcriptionURL: transcriptedFile.cloudStorageURI.href,
-    },
-  });
-  if (dbDoc === null) {
-    console.error(`Document does not exist`);
-    return res.sendStatus(400 /* Bad Request */);
-  }
-  const [file] = await transcriptedFile.download();
-  const query = `
+    // Upload raw document and transcripted file to bucket
+    const storage = new Storage({ keyFile: GCLOUD_KEY_FILEPATH! });
+    const bucket = storage.bucket("quizqrafter-documents");
+    const transcriptedFile = bucket.file(`u${userId}-${filename}.transcript`);
+    const dbDoc = await prisma.document.findFirst({
+      where: {
+        transcriptionURL: transcriptedFile.cloudStorageURI.href,
+      },
+    });
+    if (dbDoc === null) {
+      console.error(`Document does not exist`);
+      return res.sendStatus(400 /* Bad Request */);
+    }
+    const [file] = await transcriptedFile.download();
+    const query = `
     You are a quiz bot. Please prioritize keeping the original precise data from the input text in your outputted quiz. You don't make stuff up in your study quiz. Your tasks are as follows:
 
     only output the quiz with no additional text, please.
@@ -55,93 +56,102 @@ router.post("/new", restrict, async (req, res) => {
 
     Output in markdown with the answer key at the bottom
     `;
-  const openai = new OpenAI();
-  const transcription = file.toString("utf-8");
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: query },
-      { role: "user", content: transcription },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+    const openai = new OpenAI();
+    const transcription = file.toString("utf-8");
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: query },
+        { role: "user", content: transcription },
+      ],
+      model: "gpt-3.5-turbo",
+    });
 
-  const quizContent = completion.choices[0].message.content ?? "";
-  if (req.headers.accept === "text/markdown") {
-    return res.status(200 /* OK */).send(quizContent);
-  }
-  const pdf = await mdToPdf({ content: quizContent });
+    const quizContent = completion.choices[0].message.content ?? "";
+    if (req.headers.accept === "text/markdown") {
+      return res.status(200 /* OK */).send(quizContent);
+    }
+    const pdf = await mdToPdf({ content: quizContent });
 
-  // Upload raw document and transcripted file to bucket
-  const quizbucket = storage.bucket("quizqrafter-quizzes");
-  const quizfile = quizbucket.file(`u${userId}-${filename}-quiz.pdf`);
-  await quizfile.save(pdf.content);
+    // Upload raw document and transcripted file to bucket
+    const quizbucket = storage.bucket("quizqrafter-quizzes");
+    const quizfile = quizbucket.file(`u${userId}-${filename}-quiz.pdf`);
+    await quizfile.save(pdf.content);
 
-  const prevQuiz = await prisma.quiz.findFirst({
-    where: {
-      documentId: dbDoc.id,
-    },
-  });
-  if (prevQuiz === null) {
-    const quiz = await prisma.quiz.create({
-      data: {
+    const prevQuiz = await prisma.quiz.findFirst({
+      where: {
         documentId: dbDoc.id,
-        urlPdf: quizfile.cloudStorageURI.href,
       },
     });
-  } else {
-    const quiz = await prisma.quiz.update({
-      where: { documentId: dbDoc.id },
-      data: {
-        urlPdf: quizfile.cloudStorageURI.href,
-      },
-    });
-  }
+    if (prevQuiz === null) {
+      const quiz = await prisma.quiz.create({
+        data: {
+          documentId: dbDoc.id,
+          urlPdf: quizfile.cloudStorageURI.href,
+        },
+      });
+    } else {
+      const quiz = await prisma.quiz.update({
+        where: { documentId: dbDoc.id },
+        data: {
+          urlPdf: quizfile.cloudStorageURI.href,
+        },
+      });
+    }
 
-  res.setHeader("Content-disposition", "attachment; filename=quiz.pdf");
-  res.setHeader("Content-type", "application/pdf");
-  res.status(200 /* OK */).send(pdf.content);
+    res.setHeader("Content-disposition", "attachment; filename=quiz.pdf");
+    res.setHeader("Content-type", "application/pdf");
+    res.status(200 /* OK */).send(pdf.content);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 router.get("/download/:filename", restrict, async (req, res) => {
-  // @ts-ignore
-  const userId = req.session.userId;
-  const user = await prisma.user.findUnique({ where: { id: userId } });
-  if (user === null) {
-    // user does not exist
-    console.error(`User-${userId} does not exist`);
-    return res.sendStatus(401 /* Unauthorized */);
-  }
+  try {
+    // @ts-ignore
+    const userId = req.session.userId;
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (user === null) {
+      // user does not exist
+      console.error(`User-${userId} does not exist`);
+      return res.sendStatus(401 /* Unauthorized */);
+    }
 
-  const filename = req.params.filename;
-  const storage = new Storage({ keyFile: GCLOUD_KEY_FILEPATH! });
-  const bucket = storage.bucket("quizqrafter-documents");
-  const transcriptedFile = bucket.file(`u${userId}-${filename}.transcript`);
-  const dbDoc = await prisma.document.findFirst({
-    where: {
-      transcriptionURL: transcriptedFile.cloudStorageURI.href,
-    },
-  });
-  if (dbDoc === null) {
-    console.error(`Document does not exist`);
-    return res.sendStatus(400 /* Bad Request */);
+    const filename = req.params.filename;
+    const storage = new Storage({ keyFile: GCLOUD_KEY_FILEPATH! });
+    const bucket = storage.bucket("quizqrafter-documents");
+    const transcriptedFile = bucket.file(`u${userId}-${filename}.transcript`);
+    const dbDoc = await prisma.document.findFirst({
+      where: {
+        transcriptionURL: transcriptedFile.cloudStorageURI.href,
+      },
+    });
+    if (dbDoc === null) {
+      console.error(`Document does not exist`);
+      return res.sendStatus(400 /* Bad Request */);
+    }
+    const dbQuiz = await prisma.quiz.findUnique({
+      where: {
+        documentId: dbDoc.id,
+      },
+    });
+    if (dbQuiz === null) {
+      console.error(`Quiz does not exist`);
+      return res.sendStatus(400 /* Bad Request */);
+    }
+    const fname = dbQuiz.urlPdf.replace("gs://quizqrafter-quizzes/", ""); // eg. u1-note.pdf-quiz.pdf
+    const [pdfQuiz] = await storage
+      .bucket("quizqrafter-quizzes")
+      .file(fname)
+      .download();
+    res.setHeader("Content-disposition", "attachment; filename=quiz.pdf");
+    res.setHeader("Content-type", "application/pdf");
+    res.status(200 /* OK */).send(pdfQuiz);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
   }
-  const dbQuiz = await prisma.quiz.findUnique({
-    where: {
-      documentId: dbDoc.id,
-    },
-  });
-  if (dbQuiz === null) {
-    console.error(`Quiz does not exist`);
-    return res.sendStatus(400 /* Bad Request */);
-  }
-  const fname = dbQuiz.urlPdf.replace("gs://quizqrafter-quizzes/", ""); // eg. u1-note.pdf-quiz.pdf
-  const [pdfQuiz] = await storage
-    .bucket("quizqrafter-quizzes")
-    .file(fname)
-    .download();
-  res.setHeader("Content-disposition", "attachment; filename=quiz.pdf");
-  res.setHeader("Content-type", "application/pdf");
-  res.status(200 /* OK */).send(pdfQuiz);
 });
 
 export default router;
