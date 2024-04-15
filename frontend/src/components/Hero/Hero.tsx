ff40913caf62ba7c5985eb33f2cc588da309a97a
JennynneJe <117150291+JennynneJe@users.noreply.github.com>
2024-04-15 18:07:39 -0400

    optimized version of landing page UI (#20)

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./hero.module.css"; // Import the CSS module
+import hImage from "../../assets/heroPic.png";
+import image2 from "../../assets/heroExtra.png";
 
 const Hero = () => {
   return (
+    <>
     <div className={styles.hero}>
+      <div className={styles.content}>
       <h1 className={styles.heroHeading}>Best online platform for learning</h1>
       <p className={styles.heroSubheading}>
-        There are many variations of passages of Lorem Ipsum available...
+        Your personal AI tutor
       </p>
       <Link to="/signup" className={styles.joinButton}>
         Get Started
       </Link>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src={hImage} alt="Hero Image" className={styles.heroImage}/>
+      </div>
     </div>
+    </>
   );
 };
 
