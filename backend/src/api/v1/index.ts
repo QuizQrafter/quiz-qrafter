b0894462c80345650f339ebd5a73927ff1ebcd4a
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-04-22 19:29:37 -0400

    configure redis for session storage

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,18 +1,43 @@
 import { Router } from "express";
-import session from "express-session";
+import session, { MemoryStore, Store } from "express-session";
+import RedisStore from "connect-redis";
+import { createClient } from "redis";
 import authRouter from "./auth";
 import documentRouter from "./document";
 import quizRouter from "./quiz";
 
-const { SESSION_SECRET = "secret" } = process.env;
+const { SESSION_SECRET = "secret", REDIS_URL } = process.env;
 
 const router = Router();
 
+let sessionStore: Store;
+if (REDIS_URL !== undefined || REDIS_URL !== "") {
+    const redisClient = createClient({
+        url: REDIS_URL,
+    });
+    redisClient.on('error', (err) => {
+        console.error('Could not establish a connection with redis. ' + err);
+    });
+    redisClient.on('connect', () => {
+        console.log('Connected to redis successfully');
+    });
+    redisClient.connect().catch((err) => console.error(err));
+    sessionStore = new RedisStore({
+        client: redisClient,
+    });
+} else {
+    sessionStore = new MemoryStore();
+}
+
 router.use(
   session({
+    store: sessionStore,
     resave: false, // don't save session if unmodified
     saveUninitialized: false,
     secret: SESSION_SECRET,
+    cookie: {
+       maxAge: 10 * 60 * 1000, // 10 minutes
+    }
   }),
 );
 
