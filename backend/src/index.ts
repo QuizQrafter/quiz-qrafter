145ae18725c688a8db11bd181d41ee9515b6cfdf
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-04-23 02:00:10 -0400

    Squashed commit of the following:
    commit 0e88409477f9c8069e6123d324d4c399e4c85073
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 01:38:08 2024 -0400
    
        (7) fix ...
    
    commit d78935b774cc2adc38361bc393366131562996f9
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 01:29:56 2024 -0400
    
        (6) fix ...
    
    commit 8c263d7a4505ceb966c42f9406706d4bdb289009
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 00:30:53 2024 -0400
    
        (5) fix ...
    
    commit 8213f73c34b984bfdb7f2aa1aa054e71143b618e
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 00:19:05 2024 -0400
    
        (4) fix ...
    
    commit ca40f26fa78afff30f5ed0d43ec9fb2481601d29
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 00:15:24 2024 -0400
    
        (3) fix ...
    
    commit 2ff41269b3dad6c532da82e37d4866bfe46e2fc7
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 00:11:35 2024 -0400
    
        (2) fix ...
    
    commit 353b6d9bea447559f52ba519e12c87f8f6edfe18
    Author: gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Tue Apr 23 00:04:53 2024 -0400
    
        fix ...
    
    commit d580ba1fd597069d769c475bcd46486d54bba347
    Author: Gideon Oludeyi <49620086+gideonoludeyi@users.noreply.github.com>
    Date:   Mon Apr 22 20:08:30 2024 -0400
    
        Merge into main (#26)
    
        * sidebar for dashboard base setup (#16)
    
        * sidebar for dashboard base setup
    
        * logo dark and light
    
        * dashboard setup with theme and upload control (#17)
    
        * dashboard setup with theme and upload control
    
        * added file identification for selected files before their uploaded
    
        * Quiz Generation (#18)
    
        * implement quiz generation
    
        * add documentation ...
    
        add documentation for running the application locally from end-to-end
    
        * add error-handling to routes
    
        handle errors in route endpoints to prevent server from crashing
    
        * add OPEN_API_KEY env var to render.yaml
    
        * store user password hash
    
        * [frontend] integrate document upload
    
        * implement quiz service
    
        * Quiz deletion
    
        * optimized version of landing page UI (#20)
    
        * handling download and forgot password (#21)
    
        * handling download and forgot password
    
        * updating login background
    
        * Settings Improvement & Cache (#23)
    
        Co-authored-by: Akshay Racch <akshayracch@Akshays-MacBook-Pro-2.local>
    
        * configure redis for session storage (#25)
    
        ---------
    
        Co-authored-by: JennynneJe <117150291+JennynneJe@users.noreply.github.com>
        Co-authored-by: Kelvin Odinamadu <55924606+Kelvin229@users.noreply.github.com>
        Co-authored-by: Luke Reinaerts <lukewillr@gmail.com>
        Co-authored-by: Luke Reinaerts <98547376+lReinaerts@users.noreply.github.com>
        Co-authored-by: ar19fa <146890335+ar19fa@users.noreply.github.com>
        Co-authored-by: Akshay Racch <akshayracch@Akshays-MacBook-Pro-2.local>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,10 @@ const { PORT = "8080", FRONTEND_URL } = process.env;
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+}
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(
