92f952305f5205b4a2457cbdd2deef5afb15f9dd
Kelvin229 <ke20od@gmail.com>
2024-04-23 06:48:53 -0400

    resolved 404 Not found error upon page refresh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import path from 'path';
 
 import v1Router from "./api/v1";
 import prisma from "./database";
@@ -11,10 +12,11 @@ const app = express();
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1) // trust first proxy
+  app.use(express.static(path.join(__dirname, 'build')));  // Serve static files from the React app's build directory
 }
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true })); // Correctly configure urlencoded to parse URL-encoded bodies
 app.use(
   cors({
     origin: FRONTEND_URL,
@@ -27,6 +29,11 @@ app.get("/healthz", (_req, res) => {
 });
 
 app.use("/api/v1", v1Router);
+
+// Catch-all route to serve React index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const server = app.listen(parseInt(PORT), () => {
   console.log(`Live on PORT ${PORT}`);
