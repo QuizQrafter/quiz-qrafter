c7f31cfc0fa2464812cd510505b79b7fdd26bae2
Gideon Oludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2025-11-04 01:27:52 -0500

    include external infra dependencies using docker-compose.yml

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
     origin: FRONTEND_URL,
